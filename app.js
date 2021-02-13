06b769a370a430d6fc063e2ec28f7e2b216c83f7
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-02-14 00:02:09 +0600

    second commit

--- app.js
+++ app.js
@@ -7,12 +7,8 @@ const sliderContainer = document.getElementById('sliders');
 // selected image 
 let sliders = [];
 
-
-// If this key doesn't work
-// Find the name in the url and go to their website
-// to create your own api key
-const KEY = '15674931-a9d714b6e9d654524df198e00&q';
-
+// api key
+const KEY = '20264298-e5a8b4fc5ab34701366ef6e15&q';
 // show images 
 const showImages = (images) => {
   imagesArea.style.display = 'block';
@@ -31,20 +27,18 @@ const showImages = (images) => {
 const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
- 
+  // call toggle image function
+  toggleImage(element);
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
-  } else {
-    alert('Hey, Already added !')
   }
 }
 var timer
@@ -52,6 +46,11 @@ const createSlider = () => {
   // check slider image length
   if (sliders.length < 2) {
     alert('Select at least 2 image.')
+    return;
+  }
+  const duration = document.getElementById('duration').value || 1000;
+  if(duration <= 0){
+    alert('Enter some positive value');
     return;
   }
   // crate slider previous next area
@@ -67,7 +66,7 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
+
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
@@ -116,7 +115,23 @@ searchBtn.addEventListener('click', function () {
   getImages(search.value)
   sliders.length = 0;
 })
+// Enter button event handler for search
+document.getElementById('search').addEventListener('keydown', event => {
+  if(event.key === 'Enter'){
+    searchBtn.click();
+  }
+})
 
 sliderBtn.addEventListener('click', function () {
   createSlider()
 })
+
+document.getElementById('duration').addEventListener('keydown', event => {
+  if(event.key === 'Enter'){
+    sliderBtn.click();
+  }
+})
+
+const toggleImage = image => {
+  image.classList.toggle('added');
+}
